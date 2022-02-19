707e52faf21f9cccad32d455f794fa0f09125c60
frame-dev <darrylhuber39@gmail.com>
2022-02-19 17:20:49 +0100

    Updated

---
+++ src/main/java/de/framedev/calculator/main/Main.java
@@ -0,0 +1,21 @@
+package de.framedev.calculator.main;
+
+/**
+ * / This Plugin was Created by FrameDev
+ * / Package : de.framedev.calculator.main
+ * / ClassName Main
+ * / Date: 19.02.22
+ * / Project: Calculator
+ * / Copyrighted by FrameDev
+ */
+public class Main {
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/de/framedev/calculator/tests/MainTest.java
@@ -0,0 +1,31 @@
+package de.framedev.calculator.tests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * / This Plugin was Created by FrameDev
+ * / Package : de.framedev.calculator.tests
+ * / ClassName MainTest
+ * / Date: 19.02.22
+ * / Project: Calculator
+ * / Copyrighted by FrameDev
+ */
+
+public class MainTest {
+
+    public MainTest() {
+    }
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void name() {
+        System.out.println("Hello World!");
+    }
+}
